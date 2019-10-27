346934613964a124172bcb32a83ed4d5ec9fa92b
Rao Zoraiz Mahmood <rzmahmood@hotmail.com>
2019-10-27 18:03:55 +1100

    Added Endpoints

---
+++ api/handleDeregisterDonor.py
@@ -0,0 +1,5 @@
+#This Function should remove the username to the database of people from which we can potentially collect blood
+
+def handleDeregisterDonor(name):
+   return "TODO: handleDeregisterdonor"
+
---
+++ api/handleScreenBlood.py
@@ -0,0 +1,3 @@
+#This function should result in the car going to the pathology and result in all carried blood going from unscreened -> screened
+def handleScreenBlood(carid):
+   return "TODO: Handle screenblood"

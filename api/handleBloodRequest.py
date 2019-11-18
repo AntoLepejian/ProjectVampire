14033589f010c1e4b5833fa565d8e78f7628728f
Rao Zoraiz Mahmood <rzmahmood@hotmail.com>
2019-11-18 23:55:51 +1100

    Added sorting blood list

--- api/handleBloodRequest.py
+++ api/handleBloodRequest.py
@@ -23,8 +23,10 @@ def handleBloodRequest(amount, hospital_name, bloodtype, db):
    for item in db:
       if (item['type'] == 'batmobile'):
          print("found batmobile!")
-         curr_blood_list = item['blood_'+bloodtype]['screened']
+         curr_blood_list = sortBloodList(item['blood_'+bloodtype]['screened'])
          #iterate through the bags available in that car of the required bloodtype
+         print(type(curr_blood_list))
+         print("===========================================\n")
          for bag in curr_blood_list[:]:
             if (currentTime < bag['expiry']):
                bloodRequired = bloodRequired - bag['blood_amount']
@@ -50,3 +52,37 @@ def handleBloodRequest(amount, hospital_name, bloodtype, db):
       return json.dumps(retMsg), db
       # return '{"value" : "partial-success", "msg" : "Partially collected {} out of {}" }'.format(collectedAmount, amount), db
      
+def sortBloodList(blood_list):
+   #ensures for all elements are sorted by blood amount
+   #,and equal blood amounts are sorted by expiry
+   i = 0
+   print("Before:\n")
+   print(blood_list)
+   # First we sort by blood amount
+   while (i < len(blood_list)):
+      counter = 0
+      while (counter < len(blood_list)-1):
+         currBag = blood_list[counter]
+         nextBag = blood_list[counter+1]
+         if (currBag["blood_amount"] < nextBag["blood_amount"]):
+            blood_list[counter], blood_list[counter+1] = nextBag, currBag
+         counter = counter + 1
+      i = i + 1
+   print("After:\n")
+   print(blood_list)
+   # Next we sort by Expiry
+   m = 0
+   while (m < len(blood_list)):
+      count = 0
+      while (count < len(blood_list)-1):
+         currBag = blood_list[count]
+         nextBag = blood_list[count+1]
+         if (currBag["blood_amount"] == nextBag["blood_amount"]):
+            if (currBag["expiry"] > nextBag["expiry"]):
+               blood_list[count], blood_list[count+1] = nextBag, currBag
+         count = count + 1
+      m = m + 1
+   print("AfterMore:\n")
+
+   print(blood_list)
+   return blood_list
